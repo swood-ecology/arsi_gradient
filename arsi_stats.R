1bfb14a3567c59196a92fea01f26bc4cb1e3666f
Stephen Wood <stephenawood@gmail.com>
2017-11-27 16:21:29 -0500

    Finalized code for analysis

--- arsi_stats.R
+++ arsi_stats.R
@@ -8,19 +8,8 @@ library(arm)        # For standardize() function
 library(rstan)      # For interfacing with Stan
 
 
-## Read in data and generate new variables
-arsi_full_data <- read_csv("~/Box Sync/Work/Writing/Manuscripts/Unsubmitted/arsi_negele/arsi_full_data.csv")
-names(arsi_full_data)[4:24] <- c('Protein','Fe.crop','Zn.crop','Area','FoodYield','WheatYield','N.appl.amt','N.appl.p.ha','SIR','POM.C','MAOM.C','POM.N','MAOM.N','Sand','Silt','Clay','pH','Cu','Fe','Mn','Zn')
-
-arsi_full_data$Protein <- arsi_full_data$Protein/100
-arsi_full_data$MAOM.C.N <- arsi_full_data$MAOM.C/arsi_full_data$MAOM.N
-arsi_full_data$POM.C.N <- arsi_full_data$POM.C/arsi_full_data$POM.N
-arsi_full_data$Fines <- arsi_full_data$Silt + arsi_full_data$Clay
-arsi_full_data$Total.N <- arsi_full_data$POM.N + arsi_full_data$MAOM.N
-
-
-## Assess correlations
-# Create function to determine correlations to plot on upper panels
+## Relevant functions
+# Determine correlations to plot on upper panels
 panel.cor <- function(x, y, digits = 2, cex.cor, ...)
 {
   usr <- par("usr"); on.exit(par(usr))
@@ -32,50 +21,98 @@ panel.cor <- function(x, y, digits = 2, cex.cor, ...)
   if(cex.final < .5) cex.final <- .6
   text(0.5, 0.5, txt, cex = cex.final)
 }
+
+## Read in data and generate new variables
+arsi_full_data <- read_csv("~/Box Sync/Work/Writing/Manuscripts/Unsubmitted/arsi_negele/arsi_full_data.csv")
+names(arsi_full_data)[4:24] <- c('Protein','Fe.crop','Zn.crop','Area','FoodYield','WheatYield','N.appl.amt','N.appl.p.ha','SIR','POM.C','MAOM.C','POM.N','MAOM.N','Sand','Silt','Clay','pH','Cu','Fe','Mn','Zn')
+
+arsi_full_data$Protein <- arsi_full_data$Protein/100
+arsi_full_data$MAOM.C.N <- arsi_full_data$MAOM.C/arsi_full_data$MAOM.N
+arsi_full_data$POM.C.N <- arsi_full_data$POM.C/arsi_full_data$POM.N
+arsi_full_data$Fines <- arsi_full_data$Silt + arsi_full_data$Clay
+# Define dummies
+arsi_full_data <- cbind(arsi_full_data, dummies::dummy(arsi_full_data$Type)) %>%
+  as.tibble()
+names(arsi_full_data)[39:41] <- c('typeForest','typeHome','typeWheat')
+
+
+## Assess correlations
 corr.vars <- c("WheatYield","N.appl.amt","POM.C","MAOM.C","POM.N","MAOM.N","Sand",
                "Silt","Clay","Cu","Fe","Mn","Zn","Dmun","Trd",
                "MAOM.C.N","POM.C.N","pH")
 pairs(arsi_full_data[,corr.vars], lower.panel = panel.smooth, upper.panel = panel.cor)
 
 
-## Yield model
-lm.yield <- lm(log(WheatYield)~MAOM.C+POM.N+N.appl.amt+Trd,data=arsi_full_data)
-summary(standardize(lm.yield))
-# car::qqPlot(lm(log(WheatYield)~MAOM.C+POM.N+Trd+N.appl.amt,data=arsi_full_data))
-# car::ncvTest(lm(log(WheatYield)~MAOM.C+POM.N+Trd+N.appl.amt,data=arsi_full_data))
-# car::residualPlots(lm(log(WheatYield)~MAOM.C+POM.N+Trd+N.appl.amt,data=arsi_full_data))
-# car::crPlots(lm(log(WheatYield)~MAOM.C+POM.N+Trd+N.appl.amt,data=arsi_full_data))
-# car::avPlots(lm(log(WheatYield)~MAOM.C+POM.N+Trd+N.appl.amt,data=arsi_full_data))
+# 1. Do yield and nutrients increase or decrease with distance to road?
+# # 1.1. Generate vector of non-text location values
+loc.name <- as.vector(yield.data$Location)
+uniq <- unique(loc.name)
+J <- length(uniq)
+loc <- rep (NA,J)
+for (i in 1:J){
+  loc[loc.name==uniq[i]] <- i
+}
 
-# Drop NA values
-yield.vars <- c("WheatYield","N.appl.amt","MAOM.C","POM.N","Trd")
+# 1.2. Define data
+yield.vars <- c("WheatYield","Fe.crop","Zn.crop","Trd","Location","N.appl.amt","pH","POM.N","MAOM.C","MAOM.N")
 yield.data <- arsi_full_data[complete.cases(arsi_full_data[,yield.vars]),c(yield.vars)]
 yield.data <- as.data.frame(yield.data)
 
-# Fit Stan model
 yield.list <- list(N=nrow(yield.data),
-                    K=4,
-                    fert=yield.data$N.appl.amt,
-                    maom=yield.data$MAOM.C,
-                    pom=yield.data$POM.N,
-                    trd=yield.data$Trd,
-                    y=yield.data$WheatYield
-                  )
-yield.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/yield.stan", data = yield.list, control = list(adapt_delta=0.99,max_treedepth=15), chains = 4)
+                   K=5,
+                   fert=yield.data$N.appl.amt,
+                   maom=yield.data$MAOM.C,
+                   pom=yield.data$POM.N,
+                   pH=yield.data$pH,
+                   trd=yield.data$Trd,
+                   y=yield.data$WheatYield
+)
+zn.list <- list(N=nrow(yield.data),
+                K=5,
+                fert=yield.data$N.appl.amt,
+                maom=yield.data$MAOM.N,
+                pom=yield.data$POM.N,
+                pH=yield.data$pH,
+                trd=yield.data$Trd,
+                y=yield.data$Zn.crop)
+fe.list <- list(N=nrow(yield.data),
+                K=5,
+                fert=yield.data$N.appl.amt,
+                maom=yield.data$MAOM.N,
+                pom=yield.data$POM.N,
+                pH=yield.data$pH,
+                trd=yield.data$Trd,
+                y=yield.data$Fe.crop)
 
-# Results
-print(yield.model, probs=c(.025,.975),
-      pars=c('beta_fert','beta_maom','beta_pom','beta_trd')) # Natural coefficients
-plot(yield.model,pars=c('beta_std','sigma_std'),outer_level=0.975)
-#traceplot(yield.model, inc_warmup = F,pars=c('beta','sigma','alpha'))
+# 1.3. Call Stan models
+yield.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/yield_and_nutrient.stan", 
+                    data = yield.list, 
+                    control = list(adapt_delta=0.99,max_treedepth=15), chains = 4)
+zn.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/yield_and_nutrients.stan", 
+                 data = zn.list, 
+                 control = list(adapt_delta=0.99,max_treedepth=15), chains = 4)
+fe.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/yield_and_nutrients.stan", 
+                 data = fe.list, 
+                 control = list(adapt_delta=0.99,max_treedepth=15), chains = 4)
 
-# Posterior predictive check
-y_pred <- rstan::extract(yield.model,pars='y_tilde')
-y_pred <- unlist(y_pred, use.names=FALSE)
+# 1.4. Model results
+print(yield.model,pars=c("beta_fert","beta_maom","beta_pom","beta_pH","beta_trd"),
+      probs=c(0.05,0.95))
+plot(yield.model,pars=c("beta_std"))
 
-yield.pp.data <- data.frame(c(y_pred,yield.list$y),c(rep("y_pred",length(y_pred)),rep("y_obs",length(yield.list$y))))
+print(zn.model,pars=c("beta_fert","beta_maom","beta_pom","beta_pH","beta_trd"),
+      probs=c(0.05,0.95))
+plot(zn.model,pars=c("beta_std"))
+
+print(fe.model,pars=c("beta_fert","beta_maom","beta_pom","beta_pH","beta_trd"),
+      probs=c(0.05,0.95))
+plot(fe.model,pars=c("beta_std"))
+
+# 1.5. Posterior predictive checks
+yield_pred <- rstan::extract(yield.model,pars='y_tilde')
+yield_pred <- unlist(yield_pred, use.names=FALSE)
+yield.pp.data <- data.frame(c(yield_pred,yield.list$y),c(rep("yield_pred",length(yield_pred)),rep("y_obs",length(yield.list$y))))
 names(yield.pp.data) <- c("y","type")
-
 ggplot(yield.pp.data, aes(x=y)) + 
   geom_density(aes(group=type, fill=type), alpha=0.75) + theme_bw() +
   xlab("Wheat yield") + ylab("Density") +
@@ -88,37 +125,10 @@ ggplot(yield.pp.data, aes(x=y)) +
     plot.background = element_rect(fill="white")
   )
 
-
-## Nutrient models
-# Initial models
-lm.zn <- lm(log(Zn.crop)~ N.appl.amt + pH + MAOM.N + POM.N + Silt + Trd,data=arsi_full_data)
-summary(standardize(lm.zn))
-lm.fe <- lm(log(Fe.crop)~ N.appl.amt + pH + MAOM.N + POM.N + Silt + Trd,data=arsi_full_data)
-summary(standardize(lm.fe))
-
-# Stan models
-nutr.vars <- c("Zn.crop","Fe.crop","N.appl.amt","pH","Silt","MAOM.N","POM.N","Trd")
-nutr.data <- arsi_full_data[complete.cases(arsi_full_data[,nutr.vars]),c(nutr.vars)]
-nutr.data <- as.data.frame(nutr.data)
-
-zn.list <- list(N=nrow(nutr.data),
-                K=6,
-                x=nutr.data[,c('N.appl.amt','pH','Silt','MAOM.N','POM.N','Trd')],
-                y=nutr.data$Zn.crop
-)
-
-zn.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/crop_nutrients.stan", data = zn.list, control = list(adapt_delta=0.99,max_treedepth=17), chains=4)
-print(zn.model, probs=c(.025,.975), pars=c('beta'))
-plot(zn.model, pars=c("beta_std","sigma_std"),outer_level=0.975)
-#traceplot(zn.model, inc_warmup = F, pars=c('alpha','beta','sigma'))
-
-# Posterior predictive check
 y_pred <- rstan::extract(zn.model,pars='y_tilde')
 y_pred <- unlist(y_pred, use.names=FALSE)
-
 zn.pp.data <- data.frame(c(y_pred,zn.list$y),c(rep("y_pred",length(y_pred)),rep("y_obs",length(zn.list$y))))
 names(zn.pp.data) <- c("y","type")
-
 ggplot(zn.pp.data, aes(x=y)) + 
   geom_density(aes(group=type, fill=type), alpha=0.75) + theme_bw() +
   xlab("Crop zinc concentration") + ylab("Density") +
@@ -131,24 +141,10 @@ ggplot(zn.pp.data, aes(x=y)) +
     plot.background = element_rect(fill="white")
   )
 
-fe.list <- list(N=nrow(nutr.data),
-                K=6,
-                x=nutr.data[,c('N.appl.amt','pH','Silt','MAOM.N','POM.N','Trd')],
-                y=nutr.data$Fe.crop
-)
-
-fe.model <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/crop_nutrients.stan", data = fe.list, control = list(adapt_delta=0.999,max_treedepth=20), chains=4)
-
-print(fe.model, probs=c(.025,.975), pars=c("beta"))
-plot(fe.model, pars=c("beta_std","sigma_std"),outer_level=0.975)
-
-# Posterior predictive check
 y_pred <- rstan::extract(fe.model,pars='y_tilde')
 y_pred <- unlist(y_pred, use.names=FALSE)
-
 fe.pp.data <- data.frame(c(y_pred,fe.list$y),c(rep("y_pred",length(y_pred)),rep("y_obs",length(fe.list$y))))
 names(fe.pp.data) <- c("y","type")
-
 ggplot(fe.pp.data, aes(x=y)) + 
   geom_density(aes(group=type, fill=type), alpha=0.75) + theme_bw() +
   xlab("Crop iron concentration") + ylab("Density") +
@@ -162,27 +158,15 @@ ggplot(fe.pp.data, aes(x=y)) +
   )
 
 
-## SOM models
-
-# Set up data for Stan
-# Define dummies
-arsi_full_data <- cbind(arsi_full_data, dummies::dummy(arsi_full_data$Type)) %>%
-  as.tibble()
-names(arsi_full_data)[39:41] <- c('typeForest','typeHome','typeWheat')
-
-# Compare models with different hierarchies
-lm(log(POM.C) ~ rescale(typeHome) + rescale(typeWheat) + rescale(Dmun) + rescale(Fines) + rescale(pH),arsi_full_data) %>%
-  summary()
-lme4::lmer(log(POM.C) ~ rescale(Dmun) + rescale(Fines) + rescale(pH) + (1|Type),arsi_full_data) %>%
-  summary()
-lme4::lmer(log(POM.C) ~ rescale(Dmun) + rescale(Fines) + rescale(pH) + (1 + rescale(Dmun)|Type),arsi_full_data) %>%
-  summary()
-
-# Remove NAs
+# 2. Does SOM vary along the same landscape gradient as yield? In other words, could it be a driver?
+# 2.1. Set up data for Stan
 som.vars <- c('Location','POM.C','MAOM.C','SIR','typeHome','typeWheat','Dmun','Fines','pH')
 som.data <- arsi_full_data[complete.cases(arsi_full_data[,som.vars]),c(som.vars)]
 
-# Define data
+summary(lm(POM.C~typeHome+typeWheat+Dmun+Fines+pH,data=som.data))
+summary(lm(MAOM.C~POM.C+typeHome+typeWheat+Dmun+Fines+pH,data=som.data))
+summary(lm(SIR~POM.C+MAOM.C+typeHome+typeWheat+Dmun+Fines+pH,data=som.data))
+
 pomList <- list(
   N = nrow(som.data),
   K = ncol(som.data[,c('typeHome','typeWheat','Dmun','Fines','pH')]),
@@ -202,24 +186,24 @@ sirList <- list(
   x = som.data[,c('typeHome','typeWheat','POM.C','MAOM.C','Dmun','Fines','pH')]
 )
 
-# Execute models
+# 2.2. Execute models
 POM <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/som.stan",
-     data = pomList,
-     iter = 2000, chains = 4)
-MAOM <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/som.stan",
-            data = maomList,
+            data = pomList,
             iter = 2000, chains = 4)
+MAOM <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/som.stan",
+             data = maomList,
+             iter = 2000, chains = 4)
 SIR <- stan(file = "~/Box Sync/Work/GitHub/arsi_gradient/Stan/som.stan",
             data = sirList,
             iter = 2000, chains = 4)
 
-# Print model
-print(POM,pars=c('beta'))
-plot(POM,pars=c('beta_std'),outer_level=0.975)
-print(MAOM,pars=c('beta'))
-plot(MAOM,pars=c('beta_std'),outer_level=0.975)
-print(SIR,pars=c('beta'))
-plot(SIR,pars=c('beta_std'),outer_level=0.975)
+# 2.3. Print model results
+print(POM,pars=c('beta'),probs=c(0.05,0.95))
+plot(POM,pars=c('beta_std'))
+print(MAOM,pars=c('beta'),probs=c(0.05,0.95))
+plot(MAOM,pars=c('beta_std'))
+print(SIR,pars=c('beta'),probs=c(0.05,0.95))
+plot(SIR,pars=c('beta_std'))
 
 
 # Posterior predictive checks
